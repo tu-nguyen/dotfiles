6659ac738876789606320034fce1ad3f705a9a32
Tu Nguyen <tu.nguyen@rsginc.com>
2025-05-30 17:41:05 -0400

    update setup process to check and skip if installed

--- setup.sh
+++ setup.sh
@@ -1,23 +1,53 @@
+#!/bin/bash
 
+install_mac_package() {
+  if brew list "$1" &>/dev/null; then
+    echo "$1 is already installed via brew."
+  else
+    echo "Installing $1..."
+    brew install "$@"
+  fi
+}
+
+install_linux_package() {
+  if dpkg -s "$1" &>/dev/null; then
+    echo "$1 is already installed via apt."
+  else
+    echo "Installing $1..."
+    sudo apt install -y "$@"
+  fi
+}
 
 if [[ "$(uname)" == "Darwin" ]]; then
-    brew install coreutils
-    brew install python@3.12
+  install_mac_package coreutils
+  install_mac_package python@3.12
+  # Check if powerline is installed via pip
+  if ! pip show powerline-status &>/dev/null; then
+    echo "Installing Powerline via pip..."
     pip install --user git+git://github.com/powerline/powerline
-    brew install macvim --env-std --with-override-system-vim
   else
-    sudo apt install python3-pip
-    sudo apt install git
-    sudo apt install powerline
-    sudo apt install fonts-powerline 
+    echo "Powerline is already installed (pip)."
+  fi
+
+  if brew list macvim &>/dev/null; then
+    echo "macvim is already installed."
+  else
+    echo "Installing macvim..."
+    brew install macvim --env-std --with-override-system-vim
+  fi
+
+else
+  install_linux_package python3-pip
+  install_linux_package git
+  install_linux_package powerline
+  install_linux_package fonts-powerline
 fi
 
-
-# Font
-wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
-fc-cache -vf ~/.fonts
-mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+# # Font
+# wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+# mkdir -p ~/.fonts/ && mv PowerlineSymbols.otf ~/.fonts/
+# fc-cache -vf ~/.fonts
+# mkdir -p ~/.config/fontconfig/conf.d/ && mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 
 
 

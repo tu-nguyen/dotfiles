2483e8780ea018e8cffdcb2395a3f7857e52774c
Tu Nguyen <tu.nguyen@rsginc.com>
2025-06-01 21:07:27 -0400

    start fixing powerline config

--- setup/powerline/powerline_config_setup.sh
+++ setup/powerline/powerline_config_setup.sh
@@ -42,3 +42,11 @@ else
         t WARNING "powerline-status not found. Skipping powerline setup.."
     fi
 fi
+
+if [[ -f "~/.config/powerline-shell/config.json" ]]; then
+    t "Replacing config.json for powerline-shell"
+else
+    t "Copying config.json for powerline-shell"
+fi
+
+sudo cp $DOTFILE_PATH/setup/powerline/powerline-shell-config.json  $HOME/.config/powerline-shell/config.json

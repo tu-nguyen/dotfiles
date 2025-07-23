c954b62104c805f474cbead5001cb886347721c7
Tu Nguyen <tu.nguyen@rsginc.com>
2025-07-23 09:26:28 -0400

    add fallback logic for appdata

--- setup/vscode/vscode_config_setup.sh
+++ setup/vscode/vscode_config_setup.sh
@@ -14,18 +14,25 @@ src=$DOTFILES_REPO_DIR/setup/vscode/vscode-settings.json
 
 if [[ "$OS_TYPE" == "linux" ]]; then
     t "TBD"
+
 elif [[ "$OS_TYPE" == "wsl" ]]; then
+    local raw_win_appdata=$(powershell.exe -NoProfile -NonInteractive -Command "\$Env:APPDATA" | tr -d '\r\n')
     local raw_win_userprofile=$(powershell.exe -NoProfile -NonInteractive -Command "\$Env:USERPROFILE" | tr -d '\r\n')
 
-    if [ -n "$raw_win_userprofile" ]; then
+    if [ -n "$raw_win_appdata" ]; then
+        profile_base_path=$(wslpath -u "$raw_win_appdata")/Roaming/Code/User
+    elif [ -n "$raw_win_userprofile" ]; then
         profile_base_path=$(wslpath -u "$raw_win_userprofile")/AppData/Roaming/Code/User
-        dest=$profile_base_path/settings.json
     else
         t ERROR "Could not determine Windows USERPROFILE paths." >&2
         exit 1
     fi
+
+    dest=$profile_base_path/settings.json
+
 elif [[ "$OS_TYPE" == "macos" ]]; then
     dest="$HOME/Library/Application\ Support/Code/User/settings.json"
+
 fi
 
 cp $src $dest

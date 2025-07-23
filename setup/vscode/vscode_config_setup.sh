3ca42c4b384392fc99c47616682d10e3bd116d84
Tu Nguyen <tu.nguyen@rsginc.com>
2025-07-23 09:24:37 -0400

    add cp logic for vscode on wsl

--- setup/vscode/vscode_config_setup.sh
+++ setup/vscode/vscode_config_setup.sh
@@ -9,9 +9,23 @@ else
     exit 1
 fi
 
-if [[ "$OS_TYPE" == "macos" ]]; then
-    t "Copying settings.json for VSCode"
-    cp $DOTFILES_REPO_DIR/setup/vscode/vscode-settings.json  $HOME/Library/Application\ Support/Code/User/settings.json
-else
+t "Copying settings.json for VSCode"
+src=$DOTFILES_REPO_DIR/setup/vscode/vscode-settings.json
+
+if [[ "$OS_TYPE" == "linux" ]]; then
     t "TBD"
+elif [[ "$OS_TYPE" == "wsl" ]]; then
+    local raw_win_userprofile=$(powershell.exe -NoProfile -NonInteractive -Command "\$Env:USERPROFILE" | tr -d '\r\n')
+
+    if [ -n "$raw_win_userprofile" ]; then
+        profile_base_path=$(wslpath -u "$raw_win_userprofile")/AppData/Roaming/Code/User
+        dest=$profile_base_path/settings.json
+    else
+        t ERROR "Could not determine Windows USERPROFILE paths." >&2
+        exit 1
+    fi
+elif [[ "$OS_TYPE" == "macos" ]]; then
+    dest="$HOME/Library/Application\ Support/Code/User/settings.json"
 fi
+
+cp $src $dest

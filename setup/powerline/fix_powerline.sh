d3620933e5117e60c7c64a69a906f8339291a706
tu <tu.t.nguyen93@gmail.com>
2025-08-08 08:27:15 -0400

    remove t from fix scr since it runs on root

--- setup/powerline/fix_powerline.sh
+++ setup/powerline/fix_powerline.sh
@@ -5,7 +5,7 @@ DOTFILES_CONFIG_FILE="$HOME/.bash_extras/.dotfile_config"
 if [ -f "$DOTFILES_CONFIG_FILE" ]; then
     source "$DOTFILES_CONFIG_FILE"
 else
-    t Error "Configuration file not found at $DOTFILES_CONFIG_FILE" >&2
+    echo Error "Configuration file not found at $DOTFILES_CONFIG_FILE" >&2
     exit 1
 fi
 
@@ -16,9 +16,9 @@ incorrect_line="TMUX_VAR_RE = re.compile('\$(_POWERLINE_\w+)')"
 correct_line="TMUX_VAR_RE = re.compile(r'\$(_POWERLINE_\w+)')"
 
 if grep -q "$incorrect_line" "$file_path"; then
-    t "Found incorrect line in $file_path. Correcting it..."
+    echo "Found incorrect line in $file_path. Correcting it..."
     sudo sed -i "s#${incorrect_line}#${correct_line}#g" "$file_path"
-    t "Correction complete."
+    echo "Correction complete."
 else
-    t "The line is already correct or not found. No changes made."
+    echo "The line is already correct or not found. No changes made."
 fi

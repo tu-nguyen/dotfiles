797a78ab02ab38a046dc425994077dc46cd83917
tu <tu.t.nguyen93@gmail.com>
2025-08-08 08:21:38 -0400

    add loading config to fix powerline script

--- setup/powerline/fix_powerline.sh
+++ setup/powerline/fix_powerline.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# --- Load Configuration ---
+DOTFILES_CONFIG_FILE="$HOME/.bash_extras/.dotfile_config"
+if [ -f "$DOTFILES_CONFIG_FILE" ]; then
+    source "$DOTFILES_CONFIG_FILE"
+else
+    t Error "Configuration file not found at $DOTFILES_CONFIG_FILE" >&2
+    exit 1
+fi
+
 POWERLINE_PATH=$(pip show powerline-status 2>/dev/null | grep '^Location:' | cut -d' ' -f2-)
 file_path="$POWERLINE_PATH/powerline/bindings/config.py"
 
@@ -9,7 +18,7 @@ correct_line="TMUX_VAR_RE = re.compile(r'\$(_POWERLINE_\w+)')"
 if grep -q "$incorrect_line" "$file_path"; then
     t "Found incorrect line in $file_path. Correcting it..."
     sudo sed -i "s#${incorrect_line}#${correct_line}#g" "$file_path"
-    echo "Correction complete."
+    t "Correction complete."
 else
     t "The line is already correct or not found. No changes made."
 fi

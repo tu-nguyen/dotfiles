853730d78366fde0879779cb2c16efec0626e70f
tu <tu.t.nguyen93@gmail.com>
2025-10-31 00:12:21 -0400

    remove extra mks

--- reference/makefile/shared/default.mk
+++
@@ -1,6 +0,0 @@
-DEFAULT_UID := $(shell id -u)
-DEFAULT_GID := $(shell id -g)
-
-# Get UID and GID from the environment or use defaults
-LOCAL_UID := $(shell id -u $(USER_NAME) 2>/dev/null || echo $(DEFAULT_UID))
-LOCAL_GID := $(shell id -g $(GROUP_NAME) 2>/dev/null || echo $(DEFAULT_GID))
